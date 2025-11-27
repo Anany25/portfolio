597a8d6973b6f7deca83e1241e759bf68e042350
Anany25 <ananykumarsingh25@gmail.com>
2025-11-26 22:25:52 -0500

    Updates

--- src/services/yearInReviewService.js
+++
@@ -1,9 +0,0 @@
-const yearInReviews = [];
-
-export const fetchYearInReviews = async () => {
-  return yearInReviews;
-};
-
-export const fetchYearInReviewByLink = async (yearInReviewLink) => {
-  return yearInReviews.find((yir) => yir.yearInReviewLink === yearInReviewLink);
-};
